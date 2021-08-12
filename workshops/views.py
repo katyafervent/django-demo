c7bc694aadb35aac3f7b8d81d567ebfb87cf284b
Екатерина Чернова <katyafervent@yandex.ru>
2021-08-12 20:02:16 +0300

    Add template for the index page
    - Styles also added to the new static folder
    - View function updated to use the template

--- workshops/views.py
+++ workshops/views.py
@@ -2,4 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('This is test text for a workshop page')
+    workshops = [
+        { 
+            'title': 'A First Workshop', 
+            'location': 'New York', 
+            'slug': 'a-first-workshop' 
+        },
+        { 
+            'title': 'A Second Workshop', 
+            'location': 'Paris', 
+            'slug': 'a-second-workshop' 
+        }
+    ]
+    show_workshops = True
+    return render(request, 
+                  'workshops/index.html', 
+                  {'workshops': workshops,
+                   'show_workshops': show_workshops}
+                  )
