fe74593fda3007bad829ac1bb65054ad6b5494b3
Екатерина Чернова <katyafervent@yandex.ru>
2021-08-14 00:47:12 +0300

    Fix pep8

--- workshops/urls.py
+++ workshops/urls.py
@@ -1,9 +1,10 @@
-from django.contrib import admin
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
     path('workshops', views.index),
-    path('workshops/<slug:workshop_slug>', views.workshop_detail, name='workshop-detail'),
+    path('workshops/<slug:workshop_slug>',
+         views.workshop_detail,
+         name='workshop-detail'),
 ]
