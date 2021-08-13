9edabaad7b537a587d13ea5ee3a4120f8e04a9ea
Екатерина Чернова <katyafervent@yandex.ru>
2021-08-14 00:45:22 +0300

    Fix pep8

--- workshops/views.py
+++ workshops/views.py
@@ -1,35 +1,36 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def index(request):
     workshops = [
-        { 
-            'title': 'A First Workshop', 
-            'location': 'New York', 
-            'slug': 'a-first-workshop' 
+        {
+            'title': 'A First Workshop',
+            'location': 'New York',
+            'slug': 'a-first-workshop'
         },
-        { 
-            'title': 'A Second Workshop', 
-            'location': 'Paris', 
-            'slug': 'a-second-workshop' 
+        {
+            'title': 'A Second Workshop',
+            'location': 'Paris',
+            'slug': 'a-second-workshop'
         }
     ]
 
-    return render(request, 
-                  'workshops/index.html', 
+    return render(request,
+                  'workshops/index.html',
                   {'workshops': workshops}
                   )
+
 
 def workshop_detail(request, workshop_slug):
     print(workshop_slug)
     selected_workshop = {
-        'title': 'A First Workshop', 
+        'title': 'A First Workshop',
         'description': 'This is the first workshop!'
         }
-    return render(request, 
+    return render(request,
                   'workshops/workshop_details.html',
                   {
                       'workshop_title': selected_workshop['title'],
                       'workshop_descrioption': selected_workshop['description']
                   }
-            )
+                  )
