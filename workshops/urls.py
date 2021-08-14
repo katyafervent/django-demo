a1883b236848f6799aa2297fd45b955572c13b59
Екатерина Чернова <katyafervent@yandex.ru>
2021-08-14 19:13:05 +0300

    Add name to the index page
    Previosly was forgotten

--- workshops/urls.py
+++ workshops/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('workshops', views.index),
+    path('workshops', views.index,  name='all-workshops'),
     path('workshops/<slug:workshop_slug>',
          views.workshop_detail,
          name='workshop-detail'),
