b9e3d93b6dfc3bdeba0a177a30fce50e09b1c18e
Екатерина Чернова <katyafervent@yandex.ru>
2021-08-17 17:21:02 +0300

    Load objects from DB
    - add quering
    - update view correspondently

--- workshops/views.py
+++ workshops/views.py
@@ -1,36 +1,32 @@
 from django.shortcuts import render
 
+from .models import Workshop
+
 
 def index(request):
-    workshops = [
-        {
-            'title': 'A First Workshop',
-            'location': 'New York',
-            'slug': 'a-first-workshop'
-        },
-        {
-            'title': 'A Second Workshop',
-            'location': 'Paris',
-            'slug': 'a-second-workshop'
+    workshops = Workshop.objects.all()
+    return render(request, 'meetups/index.html', {
+        'workshops': workshops
+    })
+
+
+def workshop_details(request, workshop_slug):
+    try:
+        selected_workshop = Workshop.objects.get(slug=workshop_slug)
+        context = {
+            'workshop_found': True,
+            'workshop_title': selected_workshop.title,
+            'workshop_description': selected_workshop.description
         }
-    ]
-
-    return render(request,
-                  'workshops/index.html',
-                  {'workshops': workshops}
-                  )
-
-
-def workshop_detail(request, workshop_slug):
-    print(workshop_slug)
-    selected_workshop = {
-        'title': 'A First Workshop',
-        'description': 'This is the first workshop!'
-        }
-    return render(request,
-                  'workshops/workshop_details.html',
-                  {
-                      'workshop_title': selected_workshop['title'],
-                      'workshop_descrioption': selected_workshop['description']
-                  }
-                  )
+        return render(
+            request,
+            'workshops/workshop_details.html',
+            context
+        )
+    except Exception:
+        context = {'workshop_found': False}
+        return render(
+            request,
+            'workshops/workshop_details.html',
+            context
+        )
