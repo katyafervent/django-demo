8902ca4bbc09271df53f95819a030e706ab47d13
Екатерина Чернова <katyafervent@yandex.ru>
2021-08-17 15:11:43 +0300

    Itroduce Workshop database
    - Add initial migration file
    - Register the new database at admin interface

--- workshops/admin.py
+++ workshops/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Workshop
 
-# Register your models here.
+admin.site.register(Workshop)
--- workshops/models.py
+++ workshops/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Workshop(models.Model):
+    title = models.CharField(max_length=200)
+    organizer_email = models.EmailField()
+    date = models.DateField()
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return f'{self.title} - {self.slug}'
