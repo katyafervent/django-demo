20d201f403e4020c63848995eef9b99727c4f3f9
Екатерина Чернова <katyafervent@yandex.ru>
2021-08-17 14:40:06 +0300

    Reqister app though config name

--- demo_students/settings.py
+++ demo_students/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'workshops.apps'
+    'workshops.apps.WorkshopsConfig'
 ]
 
 MIDDLEWARE = [
