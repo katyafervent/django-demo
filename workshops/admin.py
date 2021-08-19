e60df1c69503b0258df80e3750557223b3905b80
Екатерина Чернова <katyafervent@yandex.ru>
2021-08-20 01:21:18 +0300

    Add related to workshops Models
    - Add Location Model (one-to-many relation)
    - Add Address Model (one-to-one relation with location)
    - Add Participant Model (many-to-many)

--- workshops/admin.py
+++ workshops/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Workshop
+from .models import Location
+from .models import Participant
+from .models import Address
 
 admin.site.register(Workshop)
+admin.site.register(Location)
+admin.site.register(Participant)
+admin.site.register(Address)
--- workshops/models.py
+++ workshops/models.py
@@ -1,12 +1,52 @@
 from django.db import models
 
 
+class Address(models.Model):
+    city = models.CharField(max_length=50)
+    street = models.CharField(max_length=50)
+    building = models.CharField(max_length=50)
+
+    @property
+    def address(self):
+        return f'{self.city} {self.street} {self.building}'
+
+    def __str__(self):
+        return self.address
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.OneToOneField(Address,
+                                   on_delete=models.CASCADE,
+                                   primary_key=True)
+
+    class Meta:
+        default_related_name = 'location'
+
+    def __str__(self):
+        return f'{self.name} ({self.address})'
+
+
+class Participant(models.Model):
+    email = models.EmailField(unique=True)
+    lastname = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.lastname
+
+
 class Workshop(models.Model):
     title = models.CharField(max_length=200)
     organizer_email = models.EmailField()
     date = models.DateField()
     slug = models.SlugField(unique=True)
     description = models.TextField()
+    location = models.ForeignKey(Location,
+                                 on_delete=models.CASCADE,
+                                 related_name='location',
+                                 blank=False,
+                                 null=True)
+    participants = models.ManyToManyField(Participant, blank=True)
 
     def __str__(self):
         return f'{self.title} - {self.slug}'
