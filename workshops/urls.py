8579e79a3ac09c87f7b96dfcff829f2d42152d12
Ekaterina Chernova <katyafervent@yandex.ru>
2021-12-15 16:44:45 +0300

    Update imports

--- workshops/urls.py
+++ workshops/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index,  name='index'),
-    path('workshops/', views.index,  name='all-workshops'),
+    path('', views.index, name='index'),
+    path('workshops/', views.index, name='all-workshops'),
     path('workshops/<slug:workshop_slug>',
          views.workshop_detail,
          name='workshop-detail'),
--- workshops/views.py
+++ workshops/views.py
@@ -1,36 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from .models import Workshop
 
 
 def index(request):
-    workshops = [
-        {
-            'title': 'A First Workshop',
-            'location': 'New York',
-            'slug': 'a-first-workshop'
-        },
-        {
-            'title': 'A Second Workshop',
-            'location': 'Paris',
-            'slug': 'a-second-workshop'
-        }
-    ]
-
-    return render(request,
-                  'workshops/index.html',
-                  {'workshops': workshops}
-                  )
+    workshops = Workshop.objects.all()
+    return render(request, 'workshops/index.html', {
+        'workshops': workshops
+    })
 
 
 def workshop_detail(request, workshop_slug):
-    print(workshop_slug)
-    selected_workshop = {
-        'title': 'A First Workshop',
-        'description': 'This is the first workshop!'
-        }
-    return render(request,
-                  'workshops/workshop_details.html',
-                  {
-                      'workshop_title': selected_workshop['title'],
-                      'workshop_descrioption': selected_workshop['description']
-                  }
-                  )
+    selected_workshop = get_object_or_404(Workshop, slug=workshop_slug)
+
+    context = {
+        'workshop_found': True,
+        'workshop_title': selected_workshop.title,
+        'workshop_description': selected_workshop.description
+    }
+    return render(
+        request,
+        'workshops/workshop_details.html',
+        context
+    )
