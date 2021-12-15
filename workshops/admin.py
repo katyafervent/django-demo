cb7bf2e9149afed79871dd69673ff3f86e470d32
Ekaterina Chernova <katyafervent@yandex.ru>
2021-12-15 17:03:30 +0300

    Revert "Itroduce Workshop database"
    This reverts commit 8902ca4bbc09271df53f95819a030e706ab47d13.

--- workshops/admin.py
+++ workshops/admin.py
@@ -1,5 +1,3 @@
 from django.contrib import admin
 
-from .models import Workshop
-
-admin.site.register(Workshop)
+# Register your models here.
--- workshops/models.py
+++ workshops/models.py
@@ -1,12 +1,3 @@
 from django.db import models
 
-
-class Workshop(models.Model):
-    title = models.CharField(max_length=200)
-    organizer_email = models.EmailField()
-    date = models.DateField()
-    slug = models.SlugField(unique=True)
-    description = models.TextField()
-
-    def __str__(self):
-        return f'{self.title} - {self.slug}'
+# Create your models here.
