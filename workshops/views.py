25c5bf16ac2aeffe07f63d3e320d05448948b41d
Ekaterina Chernova <katyafervent@yandex.ru>
2021-12-15 16:44:45 +0300

    Update imports

--- workshops/views.py
+++ workshops/views.py
@@ -1,32 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Workshop
 
 
 def index(request):
     workshops = Workshop.objects.all()
-    return render(request, 'meetups/index.html', {
+    return render(request, 'workshops/index.html', {
         'workshops': workshops
     })
 
 
-def workshop_details(request, workshop_slug):
-    try:
-        selected_workshop = Workshop.objects.get(slug=workshop_slug)
-        context = {
-            'workshop_found': True,
-            'workshop_title': selected_workshop.title,
-            'workshop_description': selected_workshop.description
-        }
-        return render(
-            request,
-            'workshops/workshop_details.html',
-            context
-        )
-    except Exception:
-        context = {'workshop_found': False}
-        return render(
-            request,
-            'workshops/workshop_details.html',
-            context
-        )
+def workshop_detail(request, workshop_slug):
+    selected_workshop = get_object_or_404(Workshop, slug=workshop_slug)
+
+    context = {
+        'workshop_found': True,
+        'workshop_title': selected_workshop.title,
+        'workshop_description': selected_workshop.description
+    }
+    return render(
+        request,
+        'workshops/workshop_details.html',
+        context
+    )
