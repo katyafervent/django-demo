9f729d7c4f89f350a2caf9e9533c1b6d48bd0adc
Ekaterina Chernova <katyafervent@yandex.ru>
2021-12-16 16:42:13 +0300

    Update model

--- workshops/models.py
+++ workshops/models.py
@@ -2,11 +2,5 @@ from django.db import models
 
 
 class Workshop(models.Model):
-    title = models.CharField(max_length=200)
-    organizer_email = models.EmailField()
-    date = models.DateField()
-    slug = models.SlugField(unique=True)
-    description = models.TextField()
-
-    def __str__(self):
-        return f'{self.title} - {self.slug}'
+    title = models.CharField('Название', max_length=200)
+    organizer_email = models.EmailField("Электронная почта")
