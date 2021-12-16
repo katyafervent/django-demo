133d7c26f44bbca032dad3d0d759ef9ac098991c
Ekaterina Chernova <katyafervent@yandex.ru>
2021-12-16 16:13:58 +0300

    Add vulnerability to workshop_detail for example

--- workshops/views.py
+++ workshops/views.py
@@ -5,21 +5,15 @@ from .models import Workshop
 
 def index(request):
     workshops = Workshop.objects.all()
-    return render(request, 'workshops/index.html', {
-        'workshops': workshops
-    })
+    return render(request, 'workshops/index.html', {'workshops': workshops})
 
 
 def workshop_detail(request, workshop_slug):
-    selected_workshop = get_object_or_404(Workshop, slug=workshop_slug)
+    selected_workshop = Workshop.objects.get(slug=workshop_slug)
 
     context = {
         'workshop_found': True,
         'workshop_title': selected_workshop.title,
-        'workshop_description': selected_workshop.description
+        'workshop_description': selected_workshop.description,
     }
-    return render(
-        request,
-        'workshops/workshop_details.html',
-        context
-    )
+    return render(request, 'workshops/workshop_details.html', context)
