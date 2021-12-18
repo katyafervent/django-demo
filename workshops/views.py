9d9a1ffd75d672dbac2a0af0bc234d558b509bd0
Ekaterina Chernova <efedorova@mirantis.com>
2021-12-19 01:16:10 +0300

    Add registration form

--- workshops/views.py
+++ workshops/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 
+from .forms import RegistrationForm
 from .models import Workshop
 
 
@@ -10,6 +11,13 @@ def index(request):
 
 def workshop_detail(request, workshop_slug):
     selected_workshop = get_object_or_404(Workshop, slug=workshop_slug)
+    if request.method == 'GET':
+        registration_form = RegistrationForm()
+    if request.method == 'POST':
+        registration_form = RegistrationForm(request.POST)
+        if registration_form.is_valid():
+            participant = registration_form.save()
+            selected_workshop.participants.add(participant)
 
-    context = {'workshop': selected_workshop}
+    context = {'workshop': selected_workshop, 'form': registration_form}
     return render(request, 'workshops/workshop_details.html', context)
